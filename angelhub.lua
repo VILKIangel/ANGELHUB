5ce111065e489bb1a81a9e030283fd179ce4eb02
VILKIangel <vandmidk@gmail.com>
2026-02-21 01:58:38 +0300

    Update angelhub.lua

--- angelhub.lua
+++ angelhub.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "ANGEL HUB | OWNER",
+   Name = "ANGEL HUB | Free",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "angel",
    LoadingSubtitle = "by VILKI",
